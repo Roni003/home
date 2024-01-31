128b6fe0d9dbb09ed0bae92f4e05fc9fb196bc22
Roni003 <ronikazmirci003@gmail.com>
2024-01-31 13:24:05 +0000

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import capybara from "/capybara.png";
+import Footer from "./components/footer";
 /* import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg"; */
 
 function App() {
   return (
-    <>
-      <div className=" flex gap-8 flex-col items-center justify-center min-h-screen">
+    <div id="app" className="h-svh grid grid-rows-10">
+      <div className=" flex gap-10 flex-col items-center justify-center align-middle row-span-9">
         <h1 className="">Home</h1>
         <img className="animate-spin-slow w-40" src={capybara} alt="capybara" />
       </div>
-    </>
+      <Footer className=" row-span-1 " />
+    </div>
   );
 }
 
---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import github from "/github.png";
+
+const Footer = () => {
+  return (
+    <div className=" flex justify-center">
+      <a href="https://github.com/Roni003" target="_blank" rel="noreferrer">
+        <img src={github} alt="Github Logo" />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
